009c33c0bd875e28ff8030c88fecb1122d6f9fe8
azss9 <hanikwonsone@gmail.com>
2018-11-23 00:49:39 +0800

    AJAXEDIT

--- web/admin_updatestockAJAX.php
+++ web/admin_updatestockAJAX.php
@@ -26,6 +26,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="text/javascript" src="js/memenu.js"></script>
 <script>$(document).ready(function(){$(".memenu").memenu();});</script>
 <script src="js/simpleCart.min.js"> </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 <!--header-->
@@ -87,56 +90,87 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<th>Product </th>
 			<th>Quantity </th>
 			<th>Price</th>
+			<th>Product Image</th>
+			<th></th>
 		</tr>	
 		<?php
 				 
  				$result = mysqli_query($con,"SELECT * FROM inventory");
  				while($order = mysqli_fetch_array($result)){
-	?>
-
-
+		?>
 		
 		</thead>		
 		<tbody>
-			<form action="" method="post">
-			<tr>
-			<td id="productid"><?php echo $order['product_id'] ?></td>
-			<td id="productname"><?php echo $order['product_name'] ?></td>
-			<td id="productquantity"><?php echo $order['product_quantity']?> </td>
-			<td id="productprice"><?php echo $order['product_price']?> </td>
+		
+			<!-- <form action="" method="post"> -->
+			<tr id="<?php echo $order['product_id']; ?>">
+				<td data-target="productid" id="productid"><?php echo $order['product_id'] ?></td>
+				<td data-target="productname" id="productname"><?php echo $order['product_name'] ?></td>
+				<td data-target="productquantity" id="productquantity"><?php echo $order['product_quantity']?> </td>
+				<td data-target="productprice" id="productprice"><?php echo $order['product_price']?> </td>
+				<td data-target="productimage" id="productimage"><?php echo $order['product_image']?> </td>
+				<td>
+				<a href="#" data-role="update" data-id="<?php echo $order['product_id'];?>" >Modify</a>	
+				</td>
 			</tr>
-			</form>	
+			<!-- </form> -->	
 					
 		<?php }?> 
 
 		</tbody>
 		
-		
-		
+
 		</table>
-		<div class="container">
-		<div class="activity-button">
-							<form action="admin_updatestockAJAX.php">
-							
-							<input  type="submit" value="Edit" style="float:right;margin-left:10px;background:#EF5F21;width:auto;font-size: 1.1em;
-							padding: 0.4em 0.8em;text-align: center;color: #fff;border: none;outline: none;-webkit-appearance: none;">
-							
-							</form>	
-							
-							<form action="admin_printstock.php">
-							
-							<input  type="submit" value="Print" style="float:right;margin-left:10px;background:#EF5F21;width:auto;font-size: 1.1em;
-							padding: 0.4em 0.8em;text-align: center;color: #fff;border: none;outline: none;-webkit-appearance: none;">
-							
-							</form>		 
-		</div>					
-		</div>	
-		</div>
-		</div>
+<!--BoostStrap-->
+				
+  					<!-- Modal -->
+  						<div class="modal fade" id="myModal" role="dialog">
+    						<div class="modal-dialog">
+    
+     				<!-- Modal content-->
+      					<div class="modal-content">
+        					<div class="modal-header">
+         						 <button type="button" class="close" data-dismiss="modal">&times;</button>
+         						 <h4 class="modal-title">Modify Stock</h4>
+
+                			</div>
+        				<div class="modal-body">
+        					<div class="StockUpdate">
+          						<span>Product Name</span>
+          					</div>
+          					<div>	
+          						<input type="text" id="productname" class="stockudp">	
+          					</div>
+          					<div class="StockUpdate">
+          						<span>Quantity</span>
+          					</div>	
+          					<div>	
+          						<input type="text" id="productquantity" class="stockudp">	
+          					</div>
+          					<div class="StockUpdate">
+          						<span>Price</span>
+          					</div>
+          					<div>	
+          						<input type="text" id="productprice" class="stockudp">	
+          					</div>
+          					<div class="StockUpdate">
+          						<span>Image</span>
+          					</div>
+          					<div>	
+          						<input type="text" id="productimage" class="stockudp">	
+          					</div>							        				
+          				</div>
+        				<div class="modal-footer">
+        					<button type="button" class="btn btn-default pull-right">Done</button>
+          					<button type="button" class="btn"  data-dismiss="modal">Close</button>
+        				</div>
+      					</div>
+      
+    						</div>
+  						</div>		
+</div>
+</div>
 		
-		
-	<div class="clearfix"> </div>
-	
 
 <!--//content-->
 <div class="footer">
@@ -162,5 +196,29 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 		</div>
 </body>
+
+<script>
+	$(document).ready(function()
+	{
+		//append value in input fields
+		 $(document).on('click','a[data-role=update]',function()
+		{
+			var id = $(this).data('product_id');
+			var productname = $('#'+ product_id).children('td[data-target=productname]').text();
+			var productquantity = $('#'+ product_id).children('td[data-target=productquantity]').text();
+			var productprice = $('#'+ product_id).children('td[data-target=productprice]').text();
+			var productimage = $('#'+ product_id).children('td[data-target=productimage]').text();
+			
+			$('#productname').val(productname);
+			$('#productquantity').val(productquantity);
+			$('#productprice').val(productprice);
+			$('#productimage').val(productimage);
+			$('#myModal').modal('toggle');
+					
+		})
+	});
+
+</script>
+
 </html>
 			
